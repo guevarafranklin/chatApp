d8aec22b9380fe26c7c348a23ab00d2fc382d576
guevarafranklin <tecguevara@gmail.com>
2025-01-01 19:32:52 -0600

    Finalize changes from stimulus js

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,12 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    //this.element.textContent = "Hello World!"
-    console.log("Hello from form_controller.js")
-  }
-  resetComponent()
-  {
-    
-  }
+    connect() {
+      //this.element.textContent = "Hello World!"
+      //console.log("Hello from form_controller.js")
+    }
+    resetComponent()
+    {
+      const form = this.element;
+      const timer = setInterval(() => {
+        form.reset();
+      }, 100);
+    }
 }
